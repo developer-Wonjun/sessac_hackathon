cb637c5281df859c6d2c05b35f36e3abc6041c36
Dochii <78398552+developer-Wonjun@users.noreply.github.com>
2023-06-07 15:27:15 +0900

    회원탈퇴 api 추가

--- api/users/user_api.py
+++ api/users/user_api.py
@@ -5,6 +5,7 @@ from fastapi.responses import JSONResponse
 from config.jwt_config import AuthHandler
 from database.database import db
 from model.account_model import User
+from model.scheduling_model import Schedule
 from model.user_model import *
 from schemas.user_schemas import SignUp
 
@@ -60,3 +61,26 @@ def change_info(data : SignUp,uuid=Depends(auth_handler.auth_wrapper) ,db:Sessio
         return JSONResponse({'error':e} , 400)
     
     
+@router.delete('/secession',
+    description="""
+<h1>회원 탈퇴 API</h1>
+
+---
+<h2>요구 Body</h2>
+- 없습니다. 헤더에 토큰만 넣어주세요. 
+- API 완료 이후에는, 초기 로그인 화면으로 넘어가주세요.
+             """, tags=['user'], status_code = 201)
+def user_secession(uuid=Depends(auth_handler.auth_wrapper) ,db:Session=Depends(db)):
+    
+    try:
+        user = db.query(User).filter(User.uuid == uuid)
+        user_id = user.one().id
+        db.query(Schedule).filter(Schedule.user_id == user_id).delete()
+        user.delete()
+        
+        db.commit()
+        return JSONResponse({'msg' : 'all delete.'}, status_code = 200)
+    
+    except Exception as e:
+        
+        return JSONResponse({'error':e} , 400)
